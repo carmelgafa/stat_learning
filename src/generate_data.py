8a4b7e0594ad5c3e1b52f5d489f0552980e80ac0
carmelgafa <carmelgafa@gmail.com>
2022-07-14 23:02:41 +0200

    added average plot and neighbour plots

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,24 +1,71 @@
+from turtle import color
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 error_threshold = 2
 
-x_points = []
-y_points = []
+points = pd.DataFrame(columns=['x', 'y'])
+function = pd.DataFrame(columns=['x', 'y'])
 
-for x in np.arange(0,10.2, 0.2):
+fig,ax = plt.subplots(1,1)
+
+number_of_random_points = 20
+
+resolution = 0.05
+
+for x in np.arange(0,10+resolution, resolution):
 
     # get a random number between 1 and 5
-    number_of_y_points = np.random.randint(1, 5)
+    number_of_y_points = np.random.randint(1, number_of_random_points)
+
+    y = -(np.square(x) - (10*x))
+    
+    function = function.append({'x': x, 'y': y}, ignore_index=True)
 
     for y_point in np.arange(0, number_of_y_points):
         
-        # get a random number between 0 and 0.2
+       
         error = np.random.uniform(-error_threshold, error_threshold)
-        y = -(np.square(x) - (10*x)) + error
 
-        x_points.append(x)
-        y_points.append(y)
+        points = points.append({'x': x, 'y': y+error}, ignore_index=True)
 
-plt.scatter(x_points, y_points)
+
+
+function.plot(x='x', y='y', ax=ax, kind='line', color='blue', label='function')
+
+points.plot(x='x', y='y', kind='scatter', ax=ax, color='gainsboro', label='points')
+
+
+estimate = pd.DataFrame(columns=['x', 'y'])
+
+for x in np.arange(0,10+resolution, resolution):
+    # get y points for x
+    y_points = points[points['x'] == x]
+
+    y_average = np.mean(y_points['y'])
+
+    estimate = estimate.append({'x': x, 'y': y_average}, ignore_index=True)
+
+estimate.plot(x='x', y='y', kind='line', ax=ax, color='red', label='estimate')
+
+
+neighborhood_estimate = pd.DataFrame(columns=['x', 'y'])
+
+for x in np.arange(0,10+resolution, resolution):
+    
+    window = 10*resolution
+    
+    y_points = points[(points['x'] > x - window) & (points['x'] < x + window)]
+
+    y_neighborhood_average = np.mean(y_points['y'])
+    
+    neighborhood_estimate = neighborhood_estimate.append({'x': x, 'y': y_neighborhood_average}, ignore_index=True)
+    
+neighborhood_estimate.plot(x='x', y='y', kind='line', ax=ax, color='green', label='neighborhood estimate')
+
 plt.show()
+
+
+
+
