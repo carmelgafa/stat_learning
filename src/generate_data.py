9d477e952a556471d00388eb69c64098b50312ba
carmelgafa <carmelgafa@gmail.com>
2022-07-20 23:16:24 +0200

    data extraction

--- src/generate_data.py
+++ src/generate_data.py
@@ -32,6 +32,20 @@ def generate_data(func, max_random_points=20, min_point=0, max_point=10, resolut
     # save points in csv
     points.to_csv('points.csv', index=False)
 
+    return function, points
+
+def extract_points(min_point=0, max_point=10, resolution=0.05):
+    points = pd.read_csv('points.csv')
+
+    filtered_points = pd.DataFrame(columns=['x', 'y'])
+
+    for x in np.arange(min_point,max_point+resolution, resolution):
+        y_points = points[points['x'] == x]
+        
+        filtered_points = filtered_points.append(y_points, ignore_index=True)
+
+    return filtered_points
+
 
 
 
@@ -123,6 +137,22 @@ if __name__=='__main__':
     # func = lambda x:  (10*x)
 
     func = lambda x:  (x**2) + 10
+    max_random_points=5
+    min_point=0
+    max_point=10
+    resolution=0.05
+    error_threshold=5
 
-    generate_data(func)
- 
+    function, points = generate_data(func, max_random_points, min_point, max_point, resolution, error_threshold)
+
+    resolution = resolution*20
+
+    filtered_points = extract_points(min_point, max_point, resolution)
+    
+    fig,ax = plt.subplots(1,1)
+
+    function.plot(x='x', y='y', ax=ax, kind='line', color='blue', label='function')
+
+    filtered_points.plot(x='x', y='y', kind='scatter', ax=ax, color='gainsboro', label='points')
+    
+    plt.show()
